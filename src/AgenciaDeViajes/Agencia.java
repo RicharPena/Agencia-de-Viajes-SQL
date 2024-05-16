fc60fb707916c5ce74c79da8e69b3398925ae8a7
Systema <Systema@192.168.1.55>
2024-05-16 18:22:52 -0500

    Se añadió en Agencia lo siguiente: * Un comentario * Un nuevo método llamado buscarReservas que mostrará las reservas

--- src/AgenciaDeViajes/Agencia.java
+++ src/AgenciaDeViajes/Agencia.java
@@ -7,10 +7,24 @@ package AgenciaDeViajes;
 /**
  *
  * @author Systema
+ * Se creó el método buscarReservas
+ * 
+ * Se creará un vector de ciudades (unas 20 ciudades), dependiendo de la ciudad de llegada o de partida, se tomará
+ * las posiciones de de ambos, se restarán y se tomará el valor absoluto de ellos. Este valor será un multiplicador
+ * de un precio fijo (sea por ejemplo 95 mil o 100 mil) y así se decidirá el valor del vuelo.
+ * 
+ * 
  */
 public class Agencia {
     
     public static void main(String [] args){
         
     }
+    
+    public static Reserva buscarReservas(){
+        //En el main, luego de seleccionar la reserva, se debe guardar el objeto de la reserva
+        //ya que posteriormente se va a pasar a un metodo de usuario y se corroborará
+        return null;
+    }
+    
 }
