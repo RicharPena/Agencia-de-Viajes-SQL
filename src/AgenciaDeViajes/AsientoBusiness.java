9b05c5690886b5fced121ada7583b5fac828747e
Systema <Systema@192.168.1.55>
2024-05-23 21:30:42 -0500

    Se creó la clase AsientoBusiness que tiene lo siguiente: * Se creó el constructor para esta clase * Se sobreescribió el método calcularTarifa()

---
+++ src/AgenciaDeViajes/AsientoBusiness.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AgenciaDeViajes;
+
+/**
+ *
+ * @author Systema
+ */
+public class AsientoBusiness extends Asiento{
+    
+    public AsientoBusiness(int idAsiento, boolean estado) {
+        super(idAsiento, estado);
+    }
+
+    @Override
+    public void calcularTarifa() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
